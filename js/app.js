5eee544bdba4406d67a37eaab1d7d5d5889ed8e0
Kushal Gajjar <kushal@improwised.com>
2017-08-24 19:16:23 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', []);
+app.controller('FirstController', function ($scope) {
+	$scope.title = "Registration Form";
+
+	$scope.saved = localStorage.getItem('formdata');
+	// $scope.formdata = (localStorage.getItem('formdata')!==null) ? JSON.parse($scope.saved) : [ {text: 'Learn AngularJS', done: false}, {text: 'Build an Angular app', done: false} ];
+	$scope.formdata = (localStorage.getItem('formdata')!==null) ? JSON.parse($scope.saved) : [];
+	localStorage.setItem('formdata', JSON.stringify($scope.saved));
+
+	$scope.saveForm = function () {
+		$scope.formdata.push({
+			// text: $scope.firstname,
+			fn: $scope.firstname,
+			ln: $scope.lastname,
+			em: $scope.email,
+			pn: $scope.phonenumber
+		});
+		$scope.firstname = '';
+		$scope.lastname = '';
+		$scope.email = '';
+		$scope.phonenumber = '';
+		localStorage.setItem('formdata', JSON.stringify($scope.formdata));
+	};
+});
